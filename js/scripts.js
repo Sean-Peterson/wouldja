65becabfb43d8e18fff982374067249ec8103b35
SeanPeterson <seanpeterson11@gmail.com>
2017-11-25 16:11:26 -0600

    add object creating function and prototyper

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
+var Pic = function(src, serial) {
+    this.src = src;
+    this.shown = false;
+    this.serial = serial;
+}
 
+var stack = [];
 
+var counter = new Array(17);
 
+for (var i = 0; i < counter.length; i++) {
+  var src = 'css/imgs/'+i+'.jpg';
+  var pic = new Pic(src, i);
+  stack.push(pic);
+}
+
+var runIt = function(){
+  
+}
 
 $(document).ready(function(){
-
   $('#left-btn').on({
     'click': function(){
-      // alert('Definitely not drunk yet')
-      var max = 16;
-      var min = 0;
-      var random = Math.floor(Math.random() * (max - min + 1)) + min;
-      var picture = 'css/imgs/'+random+'.jpg';
+
       $('#photo').attr('src',picture);
     }
   });
   $('#right-btn').on({
     'click': function(){
-      // alert('I would too')
       var max = 16;
       var min = 0;
       var random = Math.floor(Math.random() * (max - min + 1)) + min;
