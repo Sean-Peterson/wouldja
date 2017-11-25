97fa8d21d7925b42c2fa93e54177d6a22733a4f1
SeanPeterson <seanpeterson11@gmail.com>
2017-11-25 17:00:10 -0600

    add logic to only show pics that haven't been shown before

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,51 @@
-var Pic = function(src, serial) {
-    this.src = src;
+var Pic = function(serial) {
     this.shown = false;
     this.serial = serial;
 }
 
 var stack = [];
 
-var counter = new Array(17);
-
-for (var i = 0; i < counter.length; i++) {
-  var src = 'css/imgs/'+i+'.jpg';
-  var pic = new Pic(src, i);
-  stack.push(pic);
+var start = function(){
+  var counter = new Array(17);
+  for (var i = 0; i < counter.length; i++) {
+    var pic = new Pic(i);
+    stack[i]=pic;
+  }
 }
 
 var runIt = function(){
-  
+  var current = [];
+  for (var i = 0; i < stack.length; i++) {
+    if (stack[i].shown == false) {
+      serial = stack[i].serial;
+      pos = current.length;
+      current[pos]=serial;
+    }
+  }
+  if (current.length) {
+    var max = (current.length-1);
+    var min = 0;
+    var random = Math.floor(Math.random() * (max - min + 1)) + min;
+    var picture = 'css/imgs/'+current[random]+'.jpg';
+    console.log(picture);
+    stack[current[random]].shown=true;
+    return picture;
+  }else {
+    alert("you win")
+  }
 }
 
 $(document).ready(function(){
+  start();
   $('#left-btn').on({
     'click': function(){
-
+      var picture = runIt();
       $('#photo').attr('src',picture);
     }
   });
   $('#right-btn').on({
     'click': function(){
-      var max = 16;
-      var min = 0;
-      var random = Math.floor(Math.random() * (max - min + 1)) + min;
-      var picture = 'css/imgs/'+random+'.jpg';
+      var picture = runIt();
       $('#photo').attr('src',picture);
     }
   });
